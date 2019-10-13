606db1d001ba04a41fbb2a99fdc2091662938d60
timcodes <hardtimo@isu.edu>
2019-10-13 16:28:48 -0700

    added mroe fields to user interface

--- src/users/user.interface.ts
+++ src/users/user.interface.ts
@@ -1,10 +1,15 @@
 export interface User {
+  readonly Id: number;
   readonly firstName: string;
   readonly lastName: string;
   readonly userName: string;
   readonly email: string;
   readonly emailConfirmed: boolean;
   readonly phoneNumber: string;
+  readonly address: string;
+  readonly city: string;
+  readonly state: string;
+  readonly zip: string;
   readonly passwordHash: string;
   readonly lockOutEnabled: boolean;
   readonly lockoutEndtDate: Date;
