c1142f81a71fc482d47b8e3fb1651a6e30ee4b1e
timcodes <hardtimo@isu.edu>
2019-10-29 16:40:41 -0700

    starting on config

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { AccountsModule } from './accounts/accounts.module';
 import { RolesModule } from './roles/roles.module';
 import { MailerService } from './mailer/mailer.service';
 import { MailerModule } from './mailer/mailer.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, AccountsModule, RolesModule, MailerModule],
+  imports: [UsersModule, AuthModule, AccountsModule, RolesModule, MailerModule, ConfigModule],
   providers: [AppService, MailerService],
 })
 export class AppModule {}
